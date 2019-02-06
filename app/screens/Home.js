1ed8b9fe0f20b2e4ca60b4cd7b12451234ffc4d9
Andy Li <andylibc@gmail.com>
2019-02-06 11:57:25 -0800

    setup container

---
+++ app/screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text } from "react-native"
+
+import { Container } from "../components/Container"
+
+const Home = () => (
+  <Container>
+    <Text>Under Construction: Rain City Rewards</Text>
+  </Container>
+)
+
+export default Home
