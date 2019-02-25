f2ac30067c5662526dcd1fa4c0bbf1c5de2f9d73
Andy Li <andylibc@gmail.com>
2019-02-25 11:20:57 -0800

    add button

--- app/components/Buttons/AddFundButton.js
+++ app/components/Buttons/AddFundButton.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { TouchableOpacity, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import EStyleSheet from "react-native-extended-stylesheet"
 
 import styles from "./styles"
 
+const colors = EStyleSheet.create({
+  $orange: "$primaryOrange",
+})
+
 const AddFundButton = ({ onPress, text }) => (
   <TouchableOpacity style={styles.container} onPress={onPress}>
-    <MaterialIcons name={"add"} color={"$primaryOrange"} size={25} />
+    <MaterialIcons name={"add"} size={20} color={colors.$orange} />
     <Text style={styles.text}>{text}</Text>
   </TouchableOpacity>
 )
