72aeb993f4eeffe501e1ce09457f5c934a448b4a
Andy Li <andylibc@gmail.com>
2019-02-26 12:22:10 -0800

    modify mccfilter

--- app/screens/MccFilter.js
+++ app/screens/MccFilter.js
@@ -1,45 +1,107 @@
-import React from "react"
-import { TextInput, View } from "react-native"
+import React, { Component } from "react"
+import { FlatList, View, Modal, Platform, TouchableOpacity } from "react-native"
 import { Card } from "react-native-elements"
+import { Feather } from "@expo/vector-icons"
+import EStyleSheet from "react-native-extended-stylesheet"
 
 import AddSource from "./AddSource"
 import FundingSource from "./FundingSource"
 import { BodyText } from "../components/Text"
+import { ListItem } from "../components/ListItem"
+import { Separator } from "../components/ListItem"
+import { AddFundButton } from "../components/Buttons"
+import { Header } from "../components/Header"
 
-export default ({ id, authWith, onChange }) => {
-  const handleIdChanged = id => {
+const TEMP_MCCFILTER = ["Food", "Books", "Rent"]
+
+const colors = EStyleSheet.create({
+  $orange: "$primaryOrange",
+})
+
+class MccFilter extends Component {
+  state = {
+    modalVisible: false,
+    currentMccFilter: null,
+  }
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible })
+  }
+
+  handleMccFilterChanged = id => {
+    const { onChange, authWith } = this.props
     onChange({
       _type: "MccFilter",
       id: id,
       authWith: authWith,
     })
+    this.setState({ currentMccFilter: id })
+    this.setModalVisible(false)
   }
 
-  const handleFundingSourceChanged = data =>
+  handleFundingSourceChanged = data => {
+    const { id, authWith, onChange } = this.props
     onChange({
       _type: "MccFilter",
       id: id,
       authWith: data,
     })
+    this.setModalVisible(false)
+  }
 
-  return (
-    <Card containerStyle={{ borderRadius: 15 }}>
-      <BodyText text={"MccFilter"} />
-      <BodyText text={"Enter MccFilter id:"} />
-      <TextInput
-        style={{ backgroundColor: "#F0F0F0" }}
-        keyboardType="numeric"
-        onChangeText={handleIdChanged}
-      />
-      {id && (
-        <View>
-          {authWith ? (
-            <FundingSource {...authWith} onChange={handleFundingSourceChanged} />
-          ) : (
-            <AddSource text="Add Funding Source" onAdd={handleFundingSourceChanged} />
-          )}
+  render() {
+    const { id, authWith } = this.props
+    return (
+      <Card containerStyle={{ borderRadius: 15 }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <BodyText text={"MccFilter:"} />
+          <TouchableOpacity
+            style={{ flexDirection: "row" }}
+            onPress={() => this.setModalVisible(true)}>
+            <BodyText text={this.state.currentMccFilter} />
+            {this.state.currentMccFilter && <Feather name={"edit"} color={colors.$orange} />}
+          </TouchableOpacity>
         </View>
-      )}
-    </Card>
-  )
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => this.setModalVisible(false)}>
+          <View>
+            {Platform.OS === "ios" ? <Header /> : null}
+            <FlatList
+              data={TEMP_MCCFILTER}
+              renderItem={({ item }) => (
+                <ListItem text={item} onPress={() => this.handleMccFilterChanged(item)} />
+              )}
+              keyExtractor={item => item}
+              ItemSeparatorComponent={Separator}
+            />
+          </View>
+        </Modal>
+
+        {!this.state.currentMccFilter && (
+          <AddFundButton
+            onPress={() => {
+              this.setModalVisible(true)
+            }}
+            text="Choose MccFilter"
+          />
+        )}
+
+        {id && (
+          <View>
+            {authWith ? (
+              <FundingSource {...authWith} onChange={this.handleFundingSourceChanged} />
+            ) : (
+              <AddSource text="Add Funding Source" onAdd={this.handleFundingSourceChanged} />
+            )}
+          </View>
+        )}
+      </Card>
+    )
+  }
 }
+
+export default MccFilter
